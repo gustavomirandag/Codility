3e1b1832a51ab594255504fcaa9593a21e79d479
GustavoMG <gustavomirandag@gmail.com>
2016-10-16 05:57:41 -0200

    AbsDistinct solution

---
+++ Lessons/CaterpillarMethod/AbsDistinct.cpp
@@ -0,0 +1,40 @@
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> &A) {
+	int posL = 0;
+	int posR = (signed)A.size() - 1;
+	int countUniqueValues = 0;
+
+	if (A[posL] == -2147483648) {
+		countUniqueValues++;
+		while (posL<(signed)A.size() && A[posL] == -2147483648)
+			posL++;
+	}
+
+	while (posL <= posR) {
+		int absL = abs(A[posL]);
+		int absR = abs(A[posR]);
+
+		countUniqueValues++;
+
+		if (absL<absR) {
+			while (posR>0 && absR == abs(A[posR]))
+				posR--;
+		}
+		else if (absL > absR) {
+			while (posL<(signed)A.size() && absL == abs(A[posL]))
+				posL++;
+		}
+		else { //absL == absR
+			while (posL<(signed)A.size() && absL == abs(A[posL]))
+				posL++;
+			while (posR>0 && absR == abs(A[posR]))
+				posR--;
+		}
+
+	}
+	return countUniqueValues;
+}
